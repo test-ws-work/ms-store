cff78c5588da282ce17352d72ab5ae2f451b6845
gusferreira <gustavo.ferreira@nutrip.com.br>
2023-10-09 20:16:39 -0300

    chore: initial project config

---
+++ src/test/java/br/com/wswork/module/stores/MsStoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wswork.module.stores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
